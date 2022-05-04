35e004e56b287c50ded54ea97aaa4604f50c7028
Max Oblein <max.oblein@gmail.com>
2022-05-04 14:05:43 +0100

    created the get weather function

--- getWeather.py
+++ getWeather.py
@@ -4,34 +4,44 @@
 from urllib import response
 import requests
 
-if __name__ == "__main__":
+def getWeather(cityName, countryCode):
     apiKey = "1237eb6e810cf3862b15a8d7f7582700"
     defaultURL = "http://api.openweathermap.org/data/2.5/weather?"
 
-    #get city from commandline
-    #cityName = input("Enter city name: ")
-    cityName = "Hemel Hempstead"
-
     #query open wetaher map api
-    requestURL = defaultURL+"appid="+apiKey+"&q="+cityName
+    requestURL = defaultURL+"appid="+apiKey+"&q="+cityName+","+countryName
     response = requests.get(requestURL)
 
     #process response
     data = response.json()
 
     if data["cod"] != "404":
+        
+        #build temperature variables
         y = data["main"]
-
         current_temperature = y["temp"]
+        maxTemp = y["temp_max"]
+        minTemp = y["temp_min"]
+        humidity = y["humidity"]
+        
+        #build weather variables
         z = data["weather"]
-
         weather_description = z[0]["description"]
+        weather_icon = z[0]["icon"]
 
-        print(" Temperature (in celcius) = " +
-                        str(current_temperature-273) +
-            "\n description = " +
-                        str(weather_description))
+        #build descriptor variables
+        sys = data['sys']
+        city_name = sys['name']
+        country_code = sys['country']
+
+
+        info = {'temp':current_temperature, 'maxTemp':maxTemp, 'minTemp':minTemp, 'humidity': humidity, 'description':weather_description, 'name': city_name, 'country':country_code}
 
     else:
-        print(" City Not Found ")
+        info = {}
+
+    return info
+
+
+
     
